875c2100a963f00e778ed045b374132dbc2d8887
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2021-12-19 14:32:22 +0100

    add eslint as new linter

--- crypto-service/src/index.ts
+++ crypto-service/src/index.ts
@@ -1,16 +1,16 @@
 import express from 'express'
-import dotenv from "dotenv";
+import { Request, Response } from 'express'
+import dotenv from "dotenv"
 
 // initialize configuration
-dotenv.config();
+dotenv.config()
 const app = express()
 const port = process.env.SERVER_PORT
 
-app.get('/', (_request, response) => {
+app.get('/', (_request: Request, response: Response) => {
     response.send('Hello World!')
 })
 
 app.listen(port, () => {
-    // tslint:disable-next-line: no-console
     console.log(`Example app listening at http://localhost:${port}`)
 })
