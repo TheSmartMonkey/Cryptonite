b5ef5f934ed1b5f775842a9261bd7e60d4bd08e3
Im_Kage <myowngitrepository@gmail.com>
2021-12-21 21:56:56 +0100

    add capsha

--- ui/src/components/Home.vue
+++ ui/src/components/Home.vue
@@ -1,11 +1,15 @@
 <template>
     <section>
         Home page
+
+        <vue-recaptcha sitekey="6LejFrwdAAAAAHyJvbe2uF77QkQhkGOKtSTePO4X"></vue-recaptcha>
     </section>
 </template>
 
 <script>
+    import { VueRecaptcha } from 'vue-recaptcha';
     export default {
-        name : "Home"
+        name : "Home",
+        components: { VueRecaptcha }
     }
 </script>
