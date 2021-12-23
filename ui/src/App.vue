c22af7ecb00d2065a3d5d6ea816628cb7da40dcc
Im_Kage <myowngitrepository@gmail.com>
2021-12-23 11:35:09 +0100

    end capsha and router added

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <section>
-    <vue-recaptcha sitekey="6LejFrwdAAAAAHyJvbe2uF77QkQhkGOKtSTePO4X" @verify="onCaptchaVerified"></vue-recaptcha>
+  <section id="main-app-container">
+    <vue-recaptcha id="capsha" class="" sitekey="6LejFrwdAAAAAHyJvbe2uF77QkQhkGOKtSTePO4X" @verify="onCaptchaVerified"></vue-recaptcha>
     <router-view/>
   </section>
 
@@ -25,10 +25,27 @@
         var self = this;
         console.log("Capsha verify");
         this.ProjectManager.initRouter().then(function() {
+          var capsha = document.getElementById("capsha");
+          capsha.classList.add('hidden')
           self.$router.push("/home");
         })
       }
     }
   }
 </script>
+
+<style>
+  #main-app-container{
+    height: 100%;
+  }
+  #capsha{
+    display: flex;
+    align-items: center;
+    height: 100%;
+    justify-content: center;
+  }
+  .hidden{
+    display: none;
+  }
+</style>
 
