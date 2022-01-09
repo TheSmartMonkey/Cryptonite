f56764451cf47c4964d3de062a064b532f0c0fba
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2022-01-09 01:40:21 +0100

    simple prediction on trend

--- crypto-service/src/predictions/prediction.ts
+++ crypto-service/src/predictions/prediction.ts
@@ -1,3 +1,4 @@
+import { EOHLC } from './../models/prediction_model';
 import { IOHLC } from '../models/cryptowatcher_model';
 
 export interface IPrediction {
@@ -12,22 +13,22 @@ export interface IPrediction {
 export class Prediction {
     private ohlc: IOHLC;
     protected prediction: IPrediction;
+    protected currentPrice: number;
 
     constructor(ohlc: IOHLC) {
         this.ohlc = ohlc;
         this.prediction = this.ohlcToPrediction();
+        this.currentPrice = this.prediction['60'].slice(-1)[0][EOHLC.ClosePrice];
     }
 
-    protected applyToAllTimezone(predictionCallback: CallableFunction): IPrediction {
-        const response = {...this.prediction};
-        for (const key of Object.keys(response)) {
-            predictionCallback(key);
-        }
-        return response;
-    }
-
-    protected getTimezone(timezone: string): number[][] {
-        for (const [key, value] of Object.entries(this.ohlc)) {
+    /**
+     * Get the data from a timezone
+     * @param timezone IPrediction timezone
+     * @returns Timezone data
+     */
+    protected getTimezoneData(timezone: string): number[][] {
+        const data = {...this.ohlc};
+        for (const [key, value] of Object.entries(data)) {
             if (key ===  timezone) {
                 return value;
             }
@@ -35,6 +36,60 @@ export class Prediction {
         return [];
     }
 
+    /**
+     * Price tendance in a timezone
+     * @param openPrice OHLC model OpenPrice
+     * @param closePrice OHLC model ClosePrice
+     * @returns boolean
+     */
+    protected isPriceGoingUp(openPrice: number, closePrice: number): boolean {
+        return openPrice < closePrice ? true : false;
+    }
+
+    /**
+     * Get max value in a timezone
+     * @param data Timezone data
+     * @param elementId EOHLC timezone data id
+     * @returns max number
+     */
+    protected getMax(data: number[][], elementId: number): number {
+        let max = 0;
+        data.forEach(element => {
+            if (element[5] > max) max = element[elementId];
+        });
+        return max;
+    }
+
+    /**
+     * Get min value in a timezone
+     * @param data Timezone data
+     * @param elementId EOHLC timezone data id
+     * @returns min number
+     */
+    protected getMin(data: number[][], elementId: number): number {
+        let min = 0;
+        data.forEach(element => {
+            if (element[5] < min) min = element[elementId];
+        });
+        return min;
+    }
+
+    /**
+     * Get the average value in a timezone
+     * @param data Timezone data
+     * @param elementId EOHLC timezone data id
+     * @returns average number
+     */
+    protected getAverage(data: number[][], elementId: number): number {
+        let sum = 0;
+        data.forEach(element => sum += element[elementId]);
+        return sum / data.length;
+    }
+
+    /**
+     * Create the prediction model
+     * @returns IPrediction model
+     */
     private ohlcToPrediction(): IPrediction {
         const data = {...this.ohlc};
         return {
